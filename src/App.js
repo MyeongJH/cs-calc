c9f6fe153df57df41648bd7b4ef0b8c879416b04
MyeongJH <myeong1001@gmail.com>
2019-09-07 16:46:27 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,107 +1,94 @@
 import React, { useState } from 'react';
-import {
-  Box,
-  Button,
-  Collapsible,
-  Heading,
-  Grommet,
-  Layer,
-  ResponsiveContext
-} from 'grommet';
+import { Box, Button, Collapsible, Heading, Grommet, Layer, ResponsiveContext } from 'grommet';
 import { FormClose, Calculator } from 'grommet-icons';
 import CalcForm from './CalcForm';
 
 const theme = {
-  global: {
-    colors: {
-      brand: '#228BE6'
+    global: {
+        colors: {
+            brand: '#228BE6',
+        },
+        font: {
+            family: 'Jua',
+            size: '20px',
+            height: '20px',
+        },
     },
-    font: {
-      family: 'Jua',
-      size: '20px',
-      height: '20px'
-    }
-  }
 };
 
 const AppBar = props => (
-  <Box
-    tag="header"
-    direction="row"
-    align="center"
-    justify="between"
-    background="brand"
-    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-    elevation="medium"
-    style={{ zIndex: '1' }}
-    {...props}
-  />
+    <Box
+        tag="header"
+        direction="row"
+        align="center"
+        justify="between"
+        background="brand"
+        pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+        elevation="medium"
+        style={{ zIndex: '1' }}
+        {...props}
+    />
 );
 
 function App() {
-  const [showSidebar, setShowSidebar] = useState(false);
+    const [showSidebar, setShowSidebar] = useState(false);
 
-  return (
-    <Grommet theme={theme} full>
-      <ResponsiveContext.Consumer>
-        {size => (
-          <Box fill>
-            <AppBar>
-              <Heading level="2" margin="none">
-                회식비 계산
-              </Heading>
-              <Button
-                icon={<Calculator />}
-                onClick={() => setShowSidebar(!showSidebar)}
-              />
-            </AppBar>
-            <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
-              <Box flex align="center" justify="center">
-                <CalcForm />
-              </Box>
-              {!showSidebar || size !== 'small' ? (
-                <Collapsible direction="horizontal" open={showSidebar}>
-                  <Box
-                    flex
-                    width="medium"
-                    background="light-2"
-                    elevation="small"
-                    align="center"
-                    justify="center"
-                  >
-                    sidebar1
-                  </Box>
-                </Collapsible>
-              ) : (
-                <Layer>
-                  <Box
-                    background="light-2"
-                    tag="header"
-                    justify="end"
-                    align="center"
-                    direction="row"
-                  >
-                    <Button
-                      icon={<FormClose size='large' />}
-                      onClick={() => setShowSidebar(false)}
-                    />
-                  </Box>
-                  <Box
-                    fill
-                    background="light-2"
-                    align="center"
-                    justify="center"
-                  >
-                    sidebar2
-                  </Box>
-                </Layer>
-              )}
-            </Box>
-          </Box>
-        )}
-      </ResponsiveContext.Consumer>
-    </Grommet>
-  );
+    return (
+        <Grommet theme={theme} full>
+            <ResponsiveContext.Consumer>
+                {size => (
+                    <Box fill>
+                        <AppBar>
+                            <Heading level="2" margin="none">
+                                회식비 계산
+                            </Heading>
+                            <Button
+                                icon={<Calculator />}
+                                onClick={() => setShowSidebar(!showSidebar)}
+                            />
+                        </AppBar>
+                        <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+                            <Box flex align="center" justify="center">
+                                <CalcForm />
+                            </Box>
+                            {!showSidebar || size !== 'small' ? (
+                                <Collapsible direction="horizontal" open={showSidebar}>
+                                    <Box
+                                        flex
+                                        width="medium"
+                                        background="light-2"
+                                        elevation="small"
+                                        align="center"
+                                        justify="center"
+                                    >
+                                        sidebar1
+                                    </Box>
+                                </Collapsible>
+                            ) : (
+                                <Layer>
+                                    <Box
+                                        background="light-2"
+                                        tag="header"
+                                        justify="end"
+                                        align="center"
+                                        direction="row"
+                                    >
+                                        <Button
+                                            icon={<FormClose size="large" />}
+                                            onClick={() => setShowSidebar(false)}
+                                        />
+                                    </Box>
+                                    <Box fill background="light-2" align="center" justify="center">
+                                        sidebar2
+                                    </Box>
+                                </Layer>
+                            )}
+                        </Box>
+                    </Box>
+                )}
+            </ResponsiveContext.Consumer>
+        </Grommet>
+    );
 }
 
 export default App;
--- src/CalcForm.js
+++ src/CalcForm.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import {
-  Box,
-  Button,
-  CheckBox,
-  Form,
-  FormField,
-  RadioButtonGroup,
-  Select
-} from 'grommet';
+import { Box, Button, CheckBox, Form, FormField, RadioButtonGroup, Select } from 'grommet';
 import { Add } from 'grommet-icons';
 
 function CalcForm() {
-  return (
-    <Form>
-      <FormField name="location" label="장소" />
-      <FormField name="totalCost" label="총금액" type="number" />
-      <Button primary icon={<Add />} onClick={() => {}} />
-    </Form>
-  );
+    return (
+        <Form>
+            <FormField name="location" label="장소" />
+            <FormField name="totalCost" label="총금액" type="number" />
+            <Button primary icon={<Add />} onClick={() => {}} />
+        </Form>
+    );
 }
 
 export default CalcForm;
